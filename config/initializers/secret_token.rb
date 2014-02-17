575a0a72fb0fa9b26eb03249077f6d6529defe9d
Alex Wimbush <alex@yieldpop.com>
2014-02-17 10:25:13 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agindex::Application.config.secret_key_base = '3095b83b860d1e4e43ba221e6b2c44067f43dbcec69b80c4f83d70fdd01842d957028cefba4403342eca9690519c660209dcf61c61b80f280d00ea3b8eb8744d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Agindex::Application.config.session_store :cookie_store, key: '_agindex_session'
